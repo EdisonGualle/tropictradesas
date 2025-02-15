3cc3c2191c893971f9ffc8cfb41b40ac4ef05d9c
EdisonGualle <ed.gualle@gmail.com>
2025-02-14 19:19:17 -0500

    Agregar sombra al texto del encabezado en el componente Hero

--- src/assets/components/Hero.jsx
+++ src/assets/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
         <div className="h-[650px] bg-gray-300" >
             <div className="ps-32 bg-gray-300 ">
                 <div className="max-w-xl pt-20">
-                    <h1 className="text-[80px] text-white  font-extrabold leading-[90px]">
+                    <h1 className="text-[80px] text-white drop-shadow-lg  font-extrabold leading-[90px]">
                         EXPORTADORA DE BANANO
                     </h1>
                     <button className="bg-amber-300 py-3 px-6 mt-20 rounded-full text-2xl font-medium hover:bg-yellow-600">
